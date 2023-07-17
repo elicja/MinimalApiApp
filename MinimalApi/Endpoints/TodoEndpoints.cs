ed4bc8386e7d4ebd7f458d40a924667a3f10f750
Alicja Kowalska <niemogeznalezclaptopa@gmail.com>
2023-07-17 13:19:37 +0200

    Add project files.

---
+++ MinimalApi/Endpoints/TodoEndpoints.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TodoLibrary.DataAccess;
+
+namespace MinimalApi.Endpoints;
+
+public static class TodoEndpoints
+{
+    public static void AddTodoEndpoints(this WebApplication app)
+    {
+        app.MapGet("/api/Todos", GetAllTodos);
+        app.MapPost("/api/Todos", CreateTodo);
+        app.MapDelete("/api/Todos/{id}", DeleteTodo);
+    }
+
+    [Authorize]
+    private async static Task<IResult> GetAllTodos(ITodoData data)
+    {
+        var output = await data.GetAllAssigned(1);
+        return Results.Ok(output);
+    }
+
+    [Authorize]
+    private async static Task<IResult> CreateTodo(ITodoData data, [FromBody] string task)
+    {
+        var output = await data.Create(1, task);
+        return Results.Ok(output);
+    }
+
+    [Authorize]
+    private async static Task<IResult> DeleteTodo(ITodoData data, int id)
+    {
+        await data.Delete(1, id);
+        return Results.Ok();
+    }
+}
---
+++ MinimalApi/Program.cs
@@ -0,0 +1,47 @@
+using TodoLibrary.DataAccess;
+using MinimalApi.Endpoints;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+builder.Services.AddSingleton<ITodoData, TodoData>();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(opts =>
+    {
+        opts.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
+            ValidAudience = builder.Configuration.GetValue<string>("Authentication:Audience"),
+            IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.ASCII.GetBytes(
+                        builder.Configuration.GetValue<string>("Authentication:SecretKey")))
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.AddAuthenticationEndpoints();
+app.AddTodoEndpoints();
+
+app.Run();
